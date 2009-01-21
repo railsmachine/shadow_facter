9f0cf36d715affcddf404c2fb7ec5e7ee43eca44
Bradley Taylor <bradley@railsmachine.com>
2009-01-21 01:44:15 -0500

    updated gitignore

--- lib/shadow_facter.rb
+++ lib/shadow_facter.rb
@@ -1,16 +1,79 @@
+#--
+# Copyright 2009 Bradley Taylor <bradley@railsmachine.com>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+#--
+
 require 'facter'
 
 # To use the module helpers, 'extend ShadowFacter'.
 # Modules that extend ShadowFacter behave like standard Facter plugins. Automatically
 # namespaces the fact names based on the containing module name. Example, the RubyFacts
 # module prefixs all facts with "ruby_" in raw puppet.
-
 module ShadowFacter
   
-  # Returns lower case module name without "Facts" and appended with "_".
-  
+  # Returns lower case module name without "Facts" and appended with "::".
   def prefix
     to_s.downcase.gsub("facts", "_")
+  end
+ 
+  # Returns symbol for key with prefix.
+  def facter_key(key)
+    (prefix + key.to_s).to_sym
+  end
+
+  # Returns a fact value by key. Returns nil if non-existent or not constrained.
+  def [](key)
+    facter_fact_value(key)
+  end
+
+  # Returns the value of a Facter fact.
+  def facter_fact_value(key)
+    f = Facter[facter_key(key)]
+    f.value unless f.nil?
+  end
+  
+  # Return an array of all of the constrained fact keys.
+  def keys
+    @keys.uniq.select { |k| !facter_fact_value(k).nil? }
+  end
+
+  # Return a boolean on the availability of a fact.
+  def has_fact?(key)
+    !facter_fact_value(key).nil?
+  end
+  
+  # Return a hash of all of the constrained fact keys and values.
+  def to_hash
+    keys.inject({}) do |h, k| 
+      h[k] = facter_fact_value(k)
+      h
+    end
+  end
+  
+  # Return yaml of all of the constrained fact keys and values.
+  def to_yaml
+    require 'yaml'
+    to_hash.to_yaml
+  end
+  
+  # Return json of all of the constrained fact keys and values.
+  def to_json
+    require 'json'
+    to_hash.to_json
   end
   
   # Defines a fact in Facter using a value or block. Can be confined with a hash.
@@ -18,29 +81,19 @@ module ShadowFacter
   # Examples:
   #   fact :tea, "oolong"
   #   fact :tea, "puerh", {:season => "winter"}
-  #   fact :uptime, exec("uptime")
   #   fact(:rand) { rand }
-  
   def fact(key, value=nil, confine_args={}, &block)
-    name = key.to_s
-    facter_key =  prefix + name
-    
+    @keys ||= Array.new
+    @keys << key
+
     block = lambda { value.to_s } unless block_given?
-    Facter.add(facter_key) do 
+    Facter.add(facter_key(key)) do
       confine confine_args unless confine_args.empty?
       setcode block
     end
   end
-  
-  # Delegates to Facter and provides ModName.factname.
-  
-  def method_missing(name, *args)
-    facter_name = prefix + name.to_s
-    return Facter.method_missing(facter_name.to_sym, args)
-  end
 
   # System execute helper.
-  
   def exec(command)
     Facter::Util::Resolution.exec(command)
   end
--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -1,28 +1,74 @@
+#--
+# Copyright 2009 Bradley Taylor <bradley@railsmachine.com>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+#--
+
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe "facts module" do
-  it "creates existence check" do
-    TestFacts.drink?.should be(true)
+  it "generates a prefix" do
+    TestFacts.prefix.should == "test_"
   end
   
+  it "generates namespaced keys" do
+    TestFacts.facter_key(:key).should == :test_key
+  end
+  
+
   it "sets value using variable" do
-    TestFacts.drink.should == "tea"
+    TestFacts.has_fact?(:drink).should be(true)
+    TestFacts[:drink].should == "tea"
   end
-  
+
   it "sets facts from executed system commands" do
-    TestFacts.uname.should == `uname`.chomp
+    TestFacts.has_fact?(:uname).should be(true)
+    TestFacts[:uname].should == `uname`.chomp
   end
-  
+
   it "sets facts from blocks" do
-    TestFacts.food.should == "pizza"
+    TestFacts.has_fact?(:food).should be(true)
+    TestFacts[:food].should == "pizza"
   end
-  
-  it "namespaces facts" do
-    Facter.test_food.should == "pizza"
+
+  it "namespaces facts in Facter" do
+    Facter[:test_food].value.should == "pizza"
   end
-  
+
   it "confines setting facts" do
-    TestFacts.beer?.should be(false)
-    TestFacts.tea?.should be(true)
+    TestFacts.has_fact?(:beer).should be(false)
+    TestFacts.has_fact?(:tea).should be(true)
   end
+  
+  it "generates array of keys" do
+    TestFacts.keys.size.should == 4
+  end
+  
+  it "generates a hash of facts" do
+    TestFacts.to_hash.size.should == 4
+  end
+  
+  it "generates yaml of facts" do
+    require "yaml"
+    YAML.load(TestFacts.to_yaml).values.sort.should == TestFacts.to_hash.values.sort
+  end
+  
+  it "generate json of facts" do
+    require 'json'
+    JSON.parse(TestFacts.to_json).values.sort.should == TestFacts.to_hash.values.sort
+  end
+
 end
