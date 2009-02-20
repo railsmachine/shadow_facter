0a2b294a5d517fa59381b3de9177015a5a68e3e9
Bradley Taylor <bradley@railsmachine.com>
2009-02-20 10:00:57 -0500

    moved DSL methods out of base

--- lib/shadow_facter.rb
+++ lib/shadow_facter.rb
@@ -1,4 +1,33 @@
 require File.join(File.dirname(__FILE__) + '/shadow_facter', 'base.rb')
 
+#Return an array of defined namespaces names.
+def namespaces()
+  ShadowFacter::Base.namespaces
+end
+
+# Define a namespace.
+def namespace(name, &b)
+  ShadowFacter::Base.namespace(name, &b)
+end
+
+# Define a fact in Facter using a value or block. Can be confined with a hash
+# of Facter keys (namespace_key) and value.
+# Examples:
+#   fact :tea, "oolong"
+#   fact :tea, "puerh", {:drinks_season => "winter"}
+#   fact(:rand) { rand }
+def fact(key, value=nil, confine_args={}, &b)
+  ShadowFacter::Base.fact(key, value, confine_args, &b)
+end
+
+# Return an instance of the Facts class for the specified namespace.
+def facts(namespace)
+  ShadowFacter::Base.facts(namespace)
+end
+
+# Execute a system command via Facter.
+def exec(command)
+  Facter::Util::Resolution.exec(command)
+end
 
 
